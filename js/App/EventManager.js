055c7862fb3392a174182ab642969b08cca82d46
Ivan Demchenko <raqy.style@gmail.com>
2012-06-26 23:17:54 +0300

    On the way to proper automatization

--- js/App/EventManager.js
+++ js/App/EventManager.js
@@ -1,41 +1,42 @@
-Event = function()
-{
-    this._observers = [];
-}
-
-Event.prototype =
-{
-    raise: function (data)
-    {
-        for (var i in this._observers)
-        {
-            var item = this._observers[i];
-            item.observer.call(item.context, data);
-        }
-    },
-
-    subscribe: function (observer, context)
-    {
-        var ctx = context || null;
-        this._observers.push({ observer: observer, context: ctx });
-    },
-
-    unsubscribe: function (observer, context)
-    {
-        for (var i in this._observers)
-            if ( this._observers[i].observer == observer &&
-                 this._observers[i].context == context )
-                    delete this._observers[i];
-    }
-};
-
 App.EM = (function($){
 	
-	var _eventsArray = new Array,
+	Event = function()
+	{
+	    this._observers = [];
+	}
+	
+	Event.prototype =
+	{
+	    raise: function (data)
+	    {
+	        for (var i in this._observers)
+	        {
+	            var item = this._observers[i];
+	            item.observer.call(item.context, data);
+	        }
+	    },
+	
+	    subscribe: function (observer, context)
+	    {
+	        var ctx = context || null;
+	        this._observers.push({ observer: observer, context: ctx });
+	    },
+	
+	    unsubscribe: function (observer, context)
+	    {
+	        for (var i in this._observers)
+	            if ( this._observers[i].observer == observer &&
+	                 this._observers[i].context == context )
+	                    delete this._observers[i];
+	    }
+	};
+	
+	var
+		_eventsArray = new Array,
 		
 		_bindEvent = function(eventName, callbackFunction, context){
 			if(App.Settings.Debug.enabled)
-    			console.log('Binded: '+eventName+', with responder: '+callbackFunction);
+    			console.log( 'Binded: ' + eventName + ', with responder: ' + callbackFunction.toString().substr(0, 100)+'...' );
 			var evt;
 			if(_eventsArray[eventName]===undefined) {
 				_eventsArray[eventName] = new Event();
@@ -48,19 +49,20 @@ App.EM = (function($){
 		
 		_unbindEvent = function(eventName, callbackFunction, context)
 		{
-			if(_eventsArray[eventName]!==undefined)
+			if(_eventsArray[eventName] !== undefined) {
 				_eventsArray[eventName].unsubscribe(callbackFunction, context);
-			
+			}
 			return this;
 		},
 		
 		_triggerEvent = function(eventName, eventData)
 		{
-		    if(App.Settings.Debug.enabled)
-    		    console.log('Trigged: '+eventName+', with event data: '+(eventData == undefined ? 'no data' : stringify(eventData)));
-			if(_eventsArray[eventName]!==undefined)
+		    if(App.Settings.Debug.enabled) {
+    		    console.log( 'Trigged: ' + eventName + ', with event data: ' + (eventData == undefined ? 'no data' : stringify(eventData)) );
+    		}
+			if(_eventsArray[eventName] !== undefined) {
 				_eventsArray[eventName].raise(eventData);
-				
+			}	
 			return this;
 		},
 		
--- js/App/Ui.js
+++ js/App/Ui.js
@@ -360,4 +360,4 @@ App.Ui = (function($){
             }
         }
     }
-})(jQuery)
+}(jQuery));
